55d965a12144f11fc422553d2fceb8823a159fb2
Felipe Oliveira <fsousa@tasken.com>
2020-05-24 01:19:16 -0300

    Updates

---
+++ precache-manifest.492f2e7ab5ea8dce071f40592bb70eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e0465de9e87b67f2c0f22c18d087e6",
+    "url": "/simpleCalc/index.html"
+  },
+  {
+    "revision": "0dca2068909dc8111fc7",
+    "url": "/simpleCalc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75bf9027bb70f67d502c",
+    "url": "/simpleCalc/static/js/2.87d1e00b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpleCalc/static/js/2.87d1e00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dca2068909dc8111fc7",
+    "url": "/simpleCalc/static/js/main.d1c7faf4.chunk.js"
+  },
+  {
+    "revision": "b923bbeb507b80dd3071",
+    "url": "/simpleCalc/static/js/runtime-main.68f6017d.js"
+  }
+]);
