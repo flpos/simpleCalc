bf85326c6c26c00bdaef754ffd4205324623ba87
Felipe Oliveira <fsousa@tasken.com>
2020-05-24 00:58:34 -0300

    Melhorando legibilidade

--- src/components/Calculator/hooks/useDigits.tsx
+++ src/components/Calculator/hooks/useDigits.tsx
@@ -1,17 +1,32 @@
 import { useState, useCallback } from "react"
 
 const isNumber = (char: string) => /[0-9]/.test(char);
-const allNumber = (...chars: string[]): boolean => chars.map(v => isNumber(v)).reduce((prev, curr) => prev && curr)
+const allNumber = (...chars: string[]): boolean => { 
+    return chars
+        .map(v => isNumber(v))
+        .reduce((prev, curr) => prev && curr)
+}
 const resolveEquation = (givenOperands: string[]): string[] => {
     if (givenOperands.length === 0) return ['0'];
     if (givenOperands.length <= 2) return givenOperands;
-    let operands = isNumber(givenOperands[0]) ? ['+', ...givenOperands] : givenOperands;
-    operands = operands.length % 2 === 0 ? operands : operands.slice(-1);
+    
+    let operands = isNumber(givenOperands[0])
+        ? ['+', ...givenOperands]
+        : givenOperands;
+    operands = operands.length % 2 === 0
+        ? operands
+        : operands.slice(-1);
     let operators = operands.filter(op => !isNumber(op));
     operands = operands.filter(op => isNumber(op));
+    
     let acc = operands
-                .map((op, i) => operators[i] === '+' ? Number(op) : Number(-op))
+                .map((op, i) => {
+                    return operators[i] === '+'
+                        ? Number(op)
+                        : Number(-op)
+                })
                 .reduce((prev, curr) => prev + curr, 0)
+
     return [acc.toString()];
 }
 
